a9d547fd69f9d824c2bec4a2b90a3ad27cabaedb
Soufiane El abboubi <selabboubi55@gmail.com>
2026-04-02 18:09:03 +0100

    ex03 -- module06

--- CPP_Module_06/ex02/Base.cpp
+++ CPP_Module_06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-abbo < sel-abbo@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 00:38:19 by sel-abbo          #+#    #+#             */
-/*   Updated: 2026/03/12 20:45:32 by sel-abbo         ###   ########.fr       */
+/*   Updated: 2026/04/01 22:15:45 by sel-abbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,46 @@ void identify(Base *p)
         std::cout << "Unknown type" << std::endl;
 }
 
+void identify(Base &p)
+{
+    A a;
+    B b;
+    C c;
 
+    try
+    {
+        a = dynamic_cast<A &>(p);
+        std::cout << "A" << std::endl;
+        return;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 
+    try
+    {
+        b = dynamic_cast<B &>(p);
+        std::cout << "B" << std::endl;
+        return;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    try
+    {
+        c = dynamic_cast<C &>(p);
+        std::cout << "C" << std::endl;
+        return;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "Unknown type" << std::endl;
+}
 
 Base::~Base() {}
--- CPP_Module_06/ex02/main.cpp
+++ CPP_Module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-abbo < sel-abbo@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 00:40:14 by sel-abbo          #+#    #+#             */
-/*   Updated: 2026/03/11 01:41:50 by sel-abbo         ###   ########.fr       */
+/*   Updated: 2026/04/01 15:37:42 by sel-abbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,12 @@
 #include "C.hpp"
 
 int main()
-{}
+{
+	srand(time(NULL));
+	Base *base = generate();
+	identify(base);
+	identify(*base);
+	delete base;
+	return 0;
+}
 
